ab385ad4feabf6e883710225764e010c14f03661
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-06-16 13:09:37 -0700

    completed lab with some tests passing

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []  
+
+    if length == 1:
+        fibonacci_sequence.append(0)
+    elif length >= 2:
+        fibonacci_sequence.append(1)
+
+        for _ in range(2, length):
+            next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_number)
+
+    print(fibonacci_sequence)
+
